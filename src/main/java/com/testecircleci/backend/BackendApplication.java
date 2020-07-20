d9577332682e44ea2affc9108df26a669885dd94
Matheus Henrique Pereira Santos <matheus.santos@Matheuss-MacBook-Pro.local>
2020-07-20 14:10:01 -0300

    Pipeline ficando verde: Hello

--- src/main/java/com/testecircleci/backend/BackendApplication.java
+++ src/main/java/com/testecircleci/backend/BackendApplication.java
@@ -16,7 +16,7 @@ public class BackendApplication {
 
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello Apresentação %s!", name);
+		return String.format("Hello %s!", name);
 	}
 
 }
